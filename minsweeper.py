62804bfd67012bd7626eb4d10080c8bb449de41f
hmaqbool <95197517+hmaqbool@users.noreply.github.com>
2022-05-18 15:19:31 -0230

    Add files via upload

---
+++ minsweeper.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root=Tk()
+root.geometry('144x720')#change windo dimensions
+root.title("Minesweeper") #change title
+
+#main loop tell code to run till close "x" is pressed
+root.mainloop()
---
+++ setting.py
@@ -0,0 +1,3 @@
+#setttings
+width=1440
+height=720
