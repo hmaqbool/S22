9a9d03b7c4cda4ade1cc9a4afd56baa97ef6082e
hmaqbool <95197517+hmaqbool@users.noreply.github.com>
2022-05-20 20:58:47 -0230

    Add files via upload

---
+++ arithmetic_arranger.py
@@ -0,0 +1,18 @@
+#Arithmetic formatter
+
+def arithmetic_arranger(problems,y=True): #x is array of strings
+    answer=''
+    if len(x) > 5:
+        answer="Error: Too many problems."
+        return answer
+    operations= list(map(lambda x: x.split()[1],problems))
+    
+    
+    
+    
+    
+
+
+
+def main():
+    temp= str(input("Enter the)
